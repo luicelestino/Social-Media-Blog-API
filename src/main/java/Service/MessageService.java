5339a1313b868d70bfdf087e8c8d8030b80b9616
Luigi Celestino <luigicelestino@yahoo.com>
2025-03-30 01:07:33 +0000

    Completed implementation of MessageService.java file

--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -1,5 +1,47 @@
 package Service;
+import DAO.MessageDAO;
+import Model.Message;
+
+import java.util.*;
 
 public class MessageService {
+    // Declare MessageDAO so that the service file can use the MessageDAO methods
+    // This is because the service layer will implement the business logic
+    public MessageDAO messageDAO;
+
+    // Constructor for no-args
+    public MessageService() {
+        messageDAO = new MessageDAO();
+    }
+
+    // Constructor for when MessageDAO is provided
+    public MessageService(MessageDAO messageDAO) {
+        this.messageDAO = messageDAO;
+    }
+
+    // Use MessageDAO to retrieve all messages
+    public List<Message> getAllMessages() {
+        return messageDAO.getAllMessages();
+    }
+
+    // Use MessageDAO to retrieve messages by its ID
+    public Message getMessageByID(int message_id) {
+        return messageDAO.getMessageByID(message_id);
+    }
+
+    // Use MessageDAO to delete a message by its ID 
+    public boolean deleteMessageByID(int message_id) {
+        return messageDAO.deleteMessageByID(message_id);
+    }
+
+    // Use MessageDAO to update a message's text by its ID
+    public boolean updateMessageByID(String message_text, int message_id) {
+        return messageDAO.updateMessageByID(message_text, message_id);
+    }
+
+    // Use MessageDAO to retrieve all messages from an account's ID
+    public List<Message> getAllMessagesByAccountID(int account_id) {
+        return messageDAO.getAllMessagesByAccountID(account_id);
+    } 
 
 }
